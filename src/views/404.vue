62a825206c85ee5525130bac5715ac0bb51d0e74
annevanderaar <anne.vanderaar@student.hu.nl>
2022-03-24 23:24:19 +0100

    Update 404

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,7 +1,18 @@
 <template>
   <v-app>
     <HomeAppBar />
-    <v-main></v-main>
+    <v-main>
+      <v-container fluid class="d-flex flex-column justify-center align-center">
+        <h1 class="display-4">404</h1>
+        <!-- make background of smiley transparent -->
+        <v-img
+          src="8-82835_sad-face-emoji-png-sad-face-emoji-transparent.png"
+          width="300px"
+        ></v-img>
+        <h1>Page not found</h1>
+        <h2>Return to <a href="/" class="secondary--text">home</a></h2>
+      </v-container>
+    </v-main>
     <WebsiteFooter />
   </v-app>
 </template>
