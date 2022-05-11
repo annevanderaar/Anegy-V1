dba7811b28d3b73c3f0b3c2468e750ae44241a91
annevdaar <anne@snelwebcenter.nl>
2022-05-11 13:32:48 +0100

    made create-account and login better

--- src/views/Account/CreateAccount.vue
+++ src/views/Account/CreateAccount.vue
@@ -1,45 +1,53 @@
 <template>
   <v-app>
-    <v-main>
-      <v-card
-        class="ma-8"
-        color="#f32b2b"
-        dark
-        style="border-radius: 20px; min-height: 500px"
-      >
-        <v-img
-          alt="Logo Anegy"
-          contain
-          src="/Anegy-logo.png"
-          transition="scale-transition"
-          width="40px"
-          height="40px"
-          class=""
-        ></v-img>
-        <v-card-title>Create Account</v-card-title>
-        <v-card-subtitle>Create an account at Anegy</v-card-subtitle>
-        <v-text-field
-          :rules="[rules.required]"
-          label="Name"
-          v-model="name"
-        ></v-text-field>
-        <v-text-field
-          :rules="[rules.required]"
-          label="Email"
-          v-model="email"
-        ></v-text-field>
-        <v-text-field
-          :rules="[rules.required, rules.min]"
-          :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
-          :type="show ? 'text' : 'password'"
-          label="Password"
-          v-model="password"
-          counter
-          @click:append="show = !show"
-        ></v-text-field>
-        <v-btn>Create</v-btn>
-        <a href="#/login">Already have an account?</a>
-      </v-card>
+    <v-main style="background: #1d2021">
+      <v-container class="d-flex justify-center">
+        <v-card
+          dark
+          class="ma-8"
+          color="#919395"
+          style="border-radius: 20px; min-height: 500px; width: 40%"
+        >
+          <v-img
+            alt="Logo Anegy"
+            contain
+            src="/Anegy-logo.png"
+            transition="scale-transition"
+            width="40px"
+            height="40px"
+            class="ma-2"
+          ></v-img>
+          <v-card-title class="justify-center">Create Account</v-card-title>
+          <v-card-subtitle class="text-center"
+            >Create an account at Anegy</v-card-subtitle
+          >
+          <div class="ma-2">
+            <v-text-field
+              :rules="[rules.required]"
+              label="Name"
+              v-model="name"
+            ></v-text-field>
+            <v-text-field
+              :rules="[rules.required]"
+              label="Email"
+              v-model="email"
+            ></v-text-field>
+            <v-text-field
+              :rules="[rules.required, rules.min]"
+              :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+              :type="show ? 'text' : 'password'"
+              label="Password"
+              v-model="password"
+              counter
+              @click:append="show = !show"
+            ></v-text-field>
+          </div>
+          <div class="d-flex justify-center flex-column">
+            <v-btn class="ma-2">Create</v-btn>
+            <a href="#/login" class="ma-2">Already have an account?</a>
+          </div>
+        </v-card>
+      </v-container>
     </v-main>
   </v-app>
 </template>
--- src/views/Account/Login.vue
+++ src/views/Account/Login.vue
@@ -1,40 +1,48 @@
 <template>
   <v-app>
-    <v-main>
-      <v-card
-        class="ma-8"
-        color="#f32b2b"
-        dark
-        style="border-radius: 20px; min-height: 500px"
-      >
-        <v-img
-          alt="Logo Anegy"
-          contain
-          src="/Anegy-logo.png"
-          transition="scale-transition"
-          width="40px"
-          height="40px"
-          class=""
-        ></v-img>
-        <v-card-title>Login</v-card-title>
-        <v-card-subtitle>Login at your account on Anegy</v-card-subtitle>
-        <v-text-field
-          label="Email"
-          v-model="email"
-          :rules="[rules.required]"
-        ></v-text-field>
-        <v-text-field
-          :rules="[rules.required, rules.min]"
-          :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
-          :type="show ? 'text' : 'password'"
-          label="Password"
-          v-model="password"
-          counter
-          @click:append="show = !show"
-        ></v-text-field>
-        <v-btn>Login</v-btn>
-        <a href="">Forgot password?</a>
-      </v-card>
+    <v-main style="background: #1d2021">
+      <v-container class="d-flex justify-center">
+        <v-card
+          dark
+          class="ma-8"
+          color="#919395"
+          style="border-radius: 20px; min-height: 500px; width: 40%"
+        >
+          <v-img
+            alt="Logo Anegy"
+            contain
+            src="/Anegy-logo.png"
+            transition="scale-transition"
+            width="40px"
+            height="40px"
+            class="ma-2"
+          ></v-img>
+          <v-card-title class="justify-center">Login</v-card-title>
+          <v-card-subtitle class="text-center"
+            >Login at your account on Anegy</v-card-subtitle
+          >
+          <div class="ma-2">
+            <v-text-field
+              label="Email"
+              v-model="email"
+              :rules="[rules.required]"
+            ></v-text-field>
+            <v-text-field
+              :rules="[rules.required, rules.min]"
+              :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+              :type="show ? 'text' : 'password'"
+              label="Password"
+              v-model="password"
+              counter
+              @click:append="show = !show"
+            ></v-text-field>
+          </div>
+          <div class="d-flex justify-center flex-column">
+            <v-btn class="ma-2">Login</v-btn>
+            <a href="">Forgot password?</a>
+          </div>
+        </v-card>
+      </v-container>
     </v-main>
   </v-app>
 </template>
