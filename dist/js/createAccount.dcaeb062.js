f2da9cc8863583cfbfbd179a8c63ac0c2387c5be
annevanderaar <anne.vanderaar@student.hu.nl>
2022-05-22 15:09:45 +0200

    bug fix

--- dist/js/createAccount.dcaeb062.js
+++ dist/js/createAccount.dcaeb062.js
@@ -1,2 +1,2 @@
 (window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["createAccount"],{e753:function(n,e,t){"use strict";t.r(e);var a=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("v-app",[t("v-main")],1)},c=[],u={name:"Create Account",data:function(){return{}}},r=u,o=t("2877"),p=t("6544"),i=t.n(p),l=t("7496"),s=t("f6c4"),f=Object(o["a"])(r,a,c,!1,null,null,null);e["default"]=f.exports;i()(f,{VApp:l["a"],VMain:s["a"]})}}]);
-//# sourceMappingURL=createAccount.123660f1.js.map
+//# sourceMappingURL=createAccount.dcaeb062.js.map
